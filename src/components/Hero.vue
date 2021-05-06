f027ab780c64df42eed950c727d30c6d0eafbc27
Marvin Parada <hello@marvin.dev>
2021-05-06 13:26:46 -0700

    NEW: Create [Hero] component.

---
+++ src/components/Hero.vue
@@ -0,0 +1,38 @@
+<template>
+  <div
+    :class="[
+      'hero bg-green-500 fh-screen flex justify-center items-center text-center text-6xl md:text-8xl lg:text-9xl px-4',
+    ]"
+  >
+    <div :class="['hero__heading p-2 ']">
+      {{ title }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Hero',
+  props: {
+    title: {
+      type: String,
+      required: false,
+      default: null,
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.hero {
+  height: calc(100vh);
+
+  &__heading {
+    font-family: 'GeneralSans';
+    font-weight: bold;
+    max-width: 800px;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="home">
-    <h1>Hello! I am Marvin.</h1>
-    <p>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem earum illum inventore
-      commodi minus quae officia velit, quidem voluptate officiis perspiciatis atque aspernatur eius
-      laborum asperiores voluptatem, nobis quisquam adipisci.
-    </p>
+    <Hero id="hero" title="Front-End Web Developer" />
+    <div class="p-4">
+      <h1>Hello! I am Marvin.</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem earum illum
+        inventore commodi minus quae officia velit, quidem voluptate officiis perspiciatis atque
+        aspernatur eius laborum asperiores voluptatem, nobis quisquam adipisci.
+      </p>
+    </div>
   </div>
 </template>
 
@@ -16,6 +19,9 @@ export default Vue.extend({
   name: 'Home',
   metaInfo: {
     title: 'Home',
+  },
+  components: {
+    Hero: () => import('@/components/Hero.vue'),
   },
 });
 </script>
