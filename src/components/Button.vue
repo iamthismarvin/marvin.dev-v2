29059381787205fdbd78c0b95d8cee26fcb98423
Marvin Parada <hello@marvin.dev>
2021-05-08 13:16:51 -0700

    NEW: Create [Button] component.

---
+++ src/components/Button.vue
@@ -0,0 +1,34 @@
+<template>
+  <button
+    class="bg-gray-900 hover:bg-gray-800 border-4 border-green-500 mt-1 p-3 rounded-full text-green-500 transition-all"
+    @click="action"
+  >
+    <svg
+      class="h-8 w-8"
+      fill="none"
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" :d="icon"></path>
+    </svg>
+  </button>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Button',
+  props: {
+    action: { type: Function, required: true },
+    icon: { type: String, required: true },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+button {
+  outline: none;
+}
+</style>
--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -7,44 +7,11 @@
       <Menu class="fixed z-30" v-if="isMenuActive" />
     </transition>
     <div class="bottom-4 flex flex-col fixed right-4 z-50">
-      <button
-        class="bg-gray-900 hover:bg-gray-800 border-4 border-green-500 p-3 rounded-full text-green-500 transition-all outline-none"
-        @click="toggleMenu"
-      >
-        <svg
-          class="h-8 w-8"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M4 6h16M4 12h16M4 18h16"
-          ></path>
-        </svg>
-      </button>
-      <button
-        class="bg-gray-900 hover:bg-gray-800 border-4 border-green-500 p-3 rounded-full text-green-500 transition-all outline-none mt-1"
-        @click="toggleTerminal"
-      >
-        <svg
-          class="h-8 w-8"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
-          ></path>
-        </svg>
-      </button>
+      <Button icon="M4 6h16M4 12h16M4 18h16" :action="toggleMenu" />
+      <Button
+        icon="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+        :action="toggleTerminal"
+      />
     </div>
     <main>
       <slot />
@@ -59,6 +26,7 @@ import { mapActions, mapGetters } from 'vuex';
 export default Vue.extend({
   name: 'Default',
   components: {
+    Button: () => import('@/components/Button.vue'),
     Menu: () => import('@/components/Menu.vue'),
     Terminal: () => import('@/components/Terminal/Terminal.vue'),
   },
