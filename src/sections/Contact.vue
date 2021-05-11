4015d9a49ab9074b560b3128d5c3896d888827bd
Marvin Parada <hello@marvin.dev>
2021-05-11 15:54:44 -0700

    NEW: Add contact information.

--- src/sections/Contact.vue
+++ src/sections/Contact.vue
@@ -1,6 +1,24 @@
 <template>
-  <footer class="bg-gray-900 flex justify-center p-4 text-white">
-    <p>E-mail: <a href="mailto:hello@marvin.dev">hello@marvin.dev</a></p>
+  <footer
+    class="bg-gray-900 flex flex-col md:flex-row justify-center md:justify-around items-start md:items-center p-4 text-white h-screen"
+  >
+    <div>
+      <h2>Links</h2>
+      <p>
+        <a href="https://github.com/iamthismarvin" target="_blank" rel="noopener noreferrer"
+          >GitHub</a
+        >
+      </p>
+      <p>
+        <a href="https://codesandbox.io/u/iamthismarvin" target="_blank" rel="noopener noreferrer"
+          >CodeSandbox</a
+        >
+      </p>
+    </div>
+    <div class="mt-4">
+      <h2>Email</h2>
+      <p><a href="mailto:hello@marvin.dev">hello@marvin.dev</a></p>
+    </div>
   </footer>
 </template>
 
