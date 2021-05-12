81e4c2b8b83b73ab7c8abcb5826ea232e8f387c7
Marvin Parada <hello@marvin.dev>
2021-05-12 12:19:13 -0700

    NEW: Set Tailwind font configuration.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  theme: {},
+  theme: {
+    fontFamily: {
+      body: ['JetBrains Mono', 'sans'],
+      heading: ['GeneralSans', 'sans'],
+    },
+  },
   variants: {},
   plugins: [],
 };
