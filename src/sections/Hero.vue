c99fe3ea91a70da58b8c4d6272f3bfa7b325b72b
Marvin Parada <hello@marvin.dev>
2021-05-12 14:07:36 -0700

    NEW: Add call-to-action button to [Hero] section.

--- src/sections/Hero.vue
+++ src/sections/Hero.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="['bg-blue-50 flex flex-col lg:flex-row h-screen justify-center items-center px-4']">
+  <div :class="['bg-blue-50 flex flex-col h-screen justify-center items-center px-4']">
     <div>
       <p class="font-heading font-bold text-4xl md:text-6xl lg:text-7xl">Hello!</p>
       <p class="font-heading font-bold text-6xl md:text-8xl lg:text-9xl text-brand-green">
@@ -7,6 +7,13 @@
       </p>
       <p class="text-3xl md:text-4xl text-right">Front-End Web Developer</p>
     </div>
+    <a
+      href="mailto:hello@marvin.dev"
+      rel="noopener noreferrer"
+      class="bg-pink-600 mt-8 p-3 text-white rounded shadow-md"
+    >
+      Let's work together!
+    </a>
   </div>
 </template>
 
