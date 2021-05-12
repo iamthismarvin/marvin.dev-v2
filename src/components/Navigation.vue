985b8ca573bbd25b527519650b890132df7ba46c
Marvin Parada <hello@marvin.dev>
2021-05-12 12:24:13 -0700

    NEW: Create [Navigation] component.

---
+++ src/components/Navigation.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav class="flex justify-between p-6 top-0 w-full">
+    <img src="@/assets/images/logo.svg" alt="logo" class="h-8" />
+    <div class="font-bold text-gray-700">
+      <a href="#" class="mr-4">Projects</a>
+      <a href="#">Terminal</a>
+    </div>
+  </nav>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Navigation',
+});
+</script>
+
+<style lang="scss" scoped></style>
