dff250506525130d2b39cd3ffa9303d2fc2c7f7a
Marvin Parada <hello@marvin.dev>
2021-05-13 16:33:28 -0700

    NEW: Create [Card] component.

---
+++ src/components/Card.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="card bg-blue-50 border-gray-700 p-4 rounded shadow-lg text-gray-700">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Card',
+});
+</script>
+
+<style lang="scss" scoped>
+.card {
+  border-width: 6px;
+}
+</style>
--- src/sections/Stack.vue
+++ src/sections/Stack.vue
@@ -1,25 +1,38 @@
 <template>
-  <section class="bg-blue-50 p-4">
-    <h1>Stack</h1>
-    <div>
-      <h3>Tech</h3>
-      <TechItem
-        v-for="item in stack.tech"
-        :key="item.id"
-        :icon="item.icon"
-        :name="item.name"
-        :url="item.url"
-      />
-    </div>
-    <div>
-      <h3>Tools</h3>
-      <TechItem
-        v-for="item in stack.tools"
-        :key="item.id"
-        :icon="item.icon"
-        :name="item.name"
-        :url="item.url"
-      />
+  <section class="stack bg-blue-200 p-4 text-center text-white">
+    <h1 class="mb-2">Stack</h1>
+    <div class="flex flex-col md:flex-row flex-wrap justify-evenly">
+      <Card class="card">
+        <h3>Tech</h3>
+        <TechItem
+          v-for="item in stack.tech"
+          :key="item.id"
+          :icon="item.icon"
+          :name="item.name"
+          :url="item.url"
+          class="mb-2"
+        />
+      </Card>
+      <Card class="card">
+        <h3>Tools & OS</h3>
+        <TechItem
+          v-for="item in stack.tools"
+          :key="item.id"
+          :icon="item.icon"
+          :name="item.name"
+          :url="item.url"
+        />
+      </Card>
+      <Card class="card">
+        <h3>Familiar w/</h3>
+        <TechItem
+          v-for="item in stack.tools"
+          :key="item.id"
+          :icon="item.icon"
+          :name="item.name"
+          :url="item.url"
+        />
+      </Card>
     </div>
   </section>
 </template>
@@ -31,7 +44,14 @@ export default Vue.extend({
   name: 'Stack',
   data: () => ({
     stack: {
-      tech: [{ icon: 'vue-dot-js', name: 'Vue.JS', url: 'https://vuejs.org/' }],
+      tech: [
+        { icon: 'html5', name: 'HTML 5', url: '#' },
+        { icon: 'css3', name: 'CSS 3', url: '#' },
+        { icon: 'javascript', name: 'JavaScript', url: '#' },
+        { icon: 'typescript', name: 'TypeScript', url: 'https://www.typescriptlang.org/' },
+        { icon: 'vue-dot-js', name: 'Vue.JS', url: 'https://vuejs.org/' },
+        { icon: 'sass', name: 'Sass', url: 'https://sass-lang.com/' },
+      ],
       tools: [
         { icon: 'git', name: 'Git', url: 'https://git-scm.com/' },
         { icon: 'eslint', name: 'ESLint', url: 'https://eslint.org/' },
@@ -39,7 +59,19 @@ export default Vue.extend({
     },
   }),
   components: {
+    Card: () => import('@/components/Card.vue'),
     TechItem: () => import('@/components/TechItem.vue'),
   },
 });
 </script>
+
+<style lang="scss" scoped>
+.stack {
+  background-color: #04a89c;
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='36' height='72' viewBox='0 0 36 72'%3E%3Cg fill-rule='evenodd'%3E%3Cg fill='%2304a197' fill-opacity='1'%3E%3Cpath d='M2 6h12L8 18 2 6zm18 36h12l-6 12-6-12z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
+}
+
+.card {
+  @apply w-full md:w-72 my-2;
+}
+</style>
