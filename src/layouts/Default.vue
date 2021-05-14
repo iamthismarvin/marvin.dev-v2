2b401e6a8a8ba0b93022657289b2ba2fce866954
Marvin Parada <hello@marvin.dev>
2021-05-14 13:03:58 -0700

    EDIT: Change colors of buttons in [Default] layout to increase contrast.

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -8,7 +8,7 @@
     </transition>
     <Navigation class="absolute" />
     <div
-      class="bg-gray-900 bottom-3 flex flex-col fixed p-2 right-0 rounded-l text-brand-green z-50"
+      class="bg-brand-green bottom-3 flex flex-col fixed p-2 right-0 rounded-l shadow-lg text-white z-50"
     >
       <Button
         icon="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
